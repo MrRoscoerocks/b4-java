f8234561f8be960bcd1aaf9894f7636abb0e979f
MrRoscoerocks <roscoe.ambrose@gmail.com>
2017-04-19 14:17:58 -0600

    Add files via upload

---
+++ String_Search/src/StringSearch.java
@@ -0,0 +1,41 @@
+
+public class StringSearch {
+	
+	
+	
+	/*
+	 * method countWords will count the occurrences of word in sent there may be
+	 * many occurrences of word or none at all
+	 * indexOf(str)
+	 */
+	public static int countWords(String sent, String word) {
+		int total = 0;
+		
+		
+		
+		return total;
+	}
+
+	
+	/*
+	 * method countLetters will count the occurrences of letter in sent there
+	 * may be many occurrences of letter or none at all
+	 * substring & equals
+	 */
+	public static int countLetters(String sent, String letter) {
+		int total = 0;
+		
+		
+		for(int i = 0; i > sent.length(); i++){
+			
+		}
+		
+		
+		System.out.println(sent.substring(sent.indexOf(letter), sent.indexOf(letter)+1));
+		
+		
+		
+		return 800;
+	}
+
+}
---
+++ String_Search/src/StringSearchRunner.java
@@ -0,0 +1,24 @@
+import static java.lang.System.out;
+
+public class StringSearchRunner {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(StringSearch.countLetters("computerscience", "t"));
+		//System.out.println(StringSearch.countWords("computerscience", "to"));
+		
+		out.println();
+		System.out.println(StringSearch.countLetters("onetwoonetwofun", "n"));
+		//System.out.println(StringSearch.countWords("onefredonetwodog", "one"));
+		
+		out.println();
+		//System.out.println(StringSearch.countLetters("odogdogdogdogo", "o"));
+		//System.out.println(StringSearch.countWords("niininininininininini", "ni"));
+		
+		
+		
+
+	}
+
+}
